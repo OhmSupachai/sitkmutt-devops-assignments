1aeafff77ddc4ae4071d4727df3be7428b38976a
Supachai Thamataworn <supachai.ohm@mail.kmutt.ac.th>
2021-10-06 11:31:30 +0000

    update07

--- assignments/07-bookinfo.sh
+++ assignments/07-bookinfo.sh
@@ -1,7 +1,7 @@
-docker build ../ratings/ -t ratings
-docker build ../sitkmutt-bookinfo-details/ -t details
-docker build ../sitkmutt-bookinfo-reviews/ -t reviews
-docker build ../sitkmutt-bookinfo-productpage/ -t productpage
+docker build ./ratings/ -t ratings
+docker build ./sitkmutt-bookinfo-details/ -t details
+docker build ./sitkmutt-bookinfo-reviews/ -t reviews
+docker build ./sitkmutt-bookinfo-productpage/ -t productpage
 
 docker run -d --name mongodb -p 27017:27017 \
   -v $(pwd)/databases:/docker-entrypoint-initdb.d bitnami/mongodb:5.0.2-debian-10-r2
@@ -12,5 +12,5 @@ docker run -d -p 8081:8081 --name details details
 
 docker run -d -p 8082:9080 --name reviews --link ratings:ratings -e 'ENABLE_RATINGS=true' -e 'STAR_COLOR=yellow' -e 'RATINGS_SERVICE=http://ratings:8080' reviews
 
-docker run -d -p 8083:8083 --name productpage --link reviews:reviews --link ratings:ratings --link reviews:reviews --link details:details \
+docker run -d -p 8083:8083 --name productpage --link reviews:reviews --link ratings:ratings --link details:details \
 -e 'DETAILS_HOSTNAME=http://details:8081' -e 'RATINGS_HOSTNAME=http://ratings:8080' -e 'REVIEWS_HOSTNAME=http://reviews:9080' -e 'FLOOD_FACTOR=1' productpage
